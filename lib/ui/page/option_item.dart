d780a59750aee08824306a5b80440de027e20f49
NikhilVadoliya <vadoliya.nikhil99@gmail.com>
2020-09-15 10:16:44 +0530

    -Done Animation

--- lib/ui/page/option_item.dart
+++ lib/ui/page/option_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_selection_animation/ui/rect_clipper.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class OptionItem extends StatefulWidget {
@@ -14,74 +15,103 @@ class OptionItem extends StatefulWidget {
   _OptionItemState createState() => _OptionItemState();
 }
 
-class _OptionItemState extends State<OptionItem> {
+class _OptionItemState extends State<OptionItem> with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<int> alpha;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    alpha = IntTween(begin: 0, end: 10).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          width: double.infinity,
-          color: Colors.white24,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 5,
-                height: 70,
-                color: Colors.white70,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                widget.number,
-                style: GoogleFonts.nunito(fontSize: 25, color: Colors.white54),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                widget.option,
-                style: GoogleFonts.nunito(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
+        InkWell(
+          onTap: () {
+            _controller.forward();
+          },
+          child: Container(
+            width: double.infinity,
+            color: Colors.white24,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 5,
+                  height: 70,
+                  color: Colors.white70,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  widget.number,
+                  style:
+                      GoogleFonts.nunito(fontSize: 25, color: Colors.white54),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  widget.option,
+                  style: GoogleFonts.nunito(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          width: double.infinity,
-          color: Colors.white,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 5,
-                height: 70,
-                color: Colors.white70,
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                widget.number,
-                style: GoogleFonts.nunito(
-                    fontSize: 25, color: Color.fromRGBO(45, 71, 240, 1.0)),
-              ),
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                widget.option,
-                style: GoogleFonts.nunito(
-                    fontSize: 30,
-                    color: Color.fromRGBO(45, 71, 240, 1.0),
-                    fontWeight: FontWeight.bold),
-              )
-            ],
+        AnimatedBuilder(
+          animation: _controller,
+          child: Container(
+            width: double.infinity,
+            color: Colors.white,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 5,
+                  height: 70,
+                  color: Colors.white70,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  widget.number,
+                  style: GoogleFonts.nunito(
+                      fontSize: 25, color: Color.fromRGBO(45, 71, 240, 1.0)),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  widget.option,
+                  style: GoogleFonts.nunito(
+                      fontSize: 30,
+                      color: Color.fromRGBO(45, 71, 240, 1.0),
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
           ),
-        )
+          builder: (context, child) {
+            print('${_controller.value}====${_controller.value / 10}');
+            return ClipPath(
+              clipper: RectClipper(_controller.value),
+              child: child,
+            );
+          },
+        ),
       ],
     );
   }
